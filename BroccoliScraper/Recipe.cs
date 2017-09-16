207441f4f0a9912dc1b45183f95b3a0af59b2f2f
Kevin Trieu <dragn194@gmail.com>
2017-09-16 12:41:26 -0400

    Add framework classes

---
+++ BroccoliScraper/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroccoliScraper
+{
+
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public float Quantity { get; set; }
+    }
+
+    class Recipe
+    {
+
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+    }
+}
