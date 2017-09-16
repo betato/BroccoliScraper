edfd7eaca22a4d7056d49b0ab018c72a37144976
betato <BattlestarGatticus@gmail.com>
2017-09-16 12:32:58 -0400

    Initial Commit

---
+++ BroccoliScraper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+using System.Web;
+
+namespace BroccoliScraper
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HtmlWeb webGet = new HtmlWeb();
+            //var doc = webGet.Load("http://allrecipes.com/search/results/?wt=croissant&sort=re");
+            var doc = webGet.Load("http://www.food.com/search/croissant");
+            var root = doc.DocumentNode;
+
+            
+        }
+    }
+}
