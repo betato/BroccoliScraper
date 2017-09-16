5ccbb5341789dbe394eae29199a0ca0e3e3c2f9e
betato <BattlestarGatticus@gmail.com>
2017-09-16 16:15:22 -0400

    Add conversion factor and measurement names

---
+++ BroccoliScraper/ConversionFactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroccoliScraper
+{
+    class ConversionFactor
+    {
+        public int FoodId { get; private set; }
+        public int MeasureId { get; private set; }
+        public float Factor { get; private set; }
+
+        public ConversionFactor(string line)
+        {
+            try
+            {
+                string[] split = line.Split(',');
+                FoodId = Int32.Parse(split[0]);
+                MeasureId = Int32.Parse(split[1]);
+                Factor = float.Parse(split[2]);
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+    }
+}
---
+++ BroccoliScraper/MeasureName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroccoliScraper
+{
+    class MeasureName
+    {
+        public int MeasureId { get; private set; }
+        public string[] Description { get; private set; }
+
+        public MeasureName(string line)
+        {
+            try
+            {
+                string[] split = line.Split(',');
+                MeasureId = Int32.Parse(split[0]);
+                Description = split[1].Replace("[\" ]", "").Split(',');
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
