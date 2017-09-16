211af886fe844812829cb7602dfab8f8aacf027d
betato <BattlestarGatticus@gmail.com>
2017-09-16 16:16:16 -0400

    Add nutrient files

---
+++ BroccoliScraper/NutrientName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroccoliScraper
+{
+    class NutrientName
+    {
+        public int NutrientID { get; private set; }
+        public int NutrientCode { get; private set; }
+        public string NutrientSymbol { get; private set; }
+        public string NutrientUnit { get; private set; }
+        public string Name { get; private set; }
+
+        public NutrientName(string line)
+        {
+            try
+            {
+                string[] split = line.Split(',');
+                NutrientID = Int32.Parse(split[0]);
+                NutrientCode = Int32.Parse(split[1]);
+                NutrientSymbol = split[2];
+                NutrientUnit = split[3];
+                Name = split[4];
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
