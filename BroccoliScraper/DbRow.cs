61a1992a0a263cf8d9f5f4fa43a84a86a4ba33a7
Kevin Trieu <dragn194@gmail.com>
2017-09-16 22:37:51 -0400

    Add some data classes for database export

--- BroccoliScraper/DbRow.cs
+++ BroccoliScraper/DbRow.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace BroccoliScraper
 {
+
+    class Measure
+    {
+        public float Quantity { get; set; }
+        public UnitType Unit { get; set; }
+    }
+
+    class Nutrient
+    {
+        public string Name { get; set; }
+        public float Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+
     class DbRow
     {
-
-
-
+        public string[] Description { get; set; }
+        public UnitType MeasureType { get; set; }
+        public List<Nutrient> Nutrients { get; set; }
     }
 }
