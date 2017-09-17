d496403913fef400f6d6c4ffb6466cf640cae5cc
Kevin Trieu <dragn194@gmail.com>
2017-09-16 22:49:45 -0400

    Remove unneeded code from BroccoliScraper main

--- BroccoliScraper/Program.cs
+++ BroccoliScraper/Program.cs
@@ -18,21 +18,7 @@ namespace BroccoliScraper
             Food2ForkScraper scraper = new Food2ForkScraper();
             Recipe recipe = scraper.GetRecipe("scrambled eggs");
             Console.WriteLine(recipe.ToString());
-            FoodData data = new FoodData(@"..\..\..\cnf-fcen-csv\");
-            using (StreamWriter writer = new StreamWriter(new FileStream("out.txt", FileMode.Create, FileAccess.Write)))
-            {
-                foreach (var food in data.foods)
-                {
-                    if (food.Description != null)
-                    {
-                        writer.Write(string.Join(", ", food.Description));
-                        //Console.WriteLine(food.FoodId);
-                        writer.Write(data.getMeasure(food));
-                        writer.Write("\n");
-                    }
-                }
-            }
-            Console.ReadKey(true);
+            Console.ReadKey();
         }
     }
 }
