84c4f910019f28d944c773d60ddb59e7745d4e6d
betato <BattlestarGatticus@gmail.com>
2017-09-16 23:25:49 -0400

    Remove unused parameters from NutrientAmount

--- FoodData/NutrientAmount.cs
+++ FoodData/NutrientAmount.cs
@@ -11,26 +11,15 @@ namespace BroccoliScraper
         public int FoodId { get; private set; }
         public int NutrientID { get; private set; }
         public float NutrientValue { get; private set; }
-        public float StandardError { get; private set; }
-        public int NumberofObservations { get; private set; }
-        public int NutrientSourceID { get; private set; }
 
         public NutrientAmount(string line)
         {
-            try
-            {
+           
                 string[] split = line.Split(',');
                 FoodId = Int32.Parse(split[0]);
                 NutrientID = Int32.Parse(split[1]);
                 NutrientValue = float.Parse(split[2]);
-                StandardError = float.Parse(split[3]);
-                NumberofObservations = Int32.Parse(split[4]);
-                NutrientSourceID = Int32.Parse(split[5]);
-            }
-            catch (Exception)
-            {
-
-            }
+            
         }
     }
 }
