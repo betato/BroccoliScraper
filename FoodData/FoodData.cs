4fcb160c8008d7514473b3ac6c3a724b9bce34e4
betato <BattlestarGatticus@gmail.com>
2017-09-17 07:58:06 -0400

    Don't read description line of csv file

--- FoodData/FoodData.cs
+++ FoodData/FoodData.cs
@@ -20,9 +20,9 @@ namespace FoodData
         public FoodData(string dir)
         {
             string[] lines = File.ReadAllLines(dir + "FOOD NAME.csv", Encoding.UTF8);
-            foreach (string line in lines)
+            for (int i = 1; i < lines.Length; i++)
             {
-                FoodName name = new FoodName(line);
+                FoodName name = new FoodName(lines[i]);
                 if (name != null && name.FoodId != 0)
                 {
                     foods.Add(name.FoodId, name);
@@ -30,9 +30,9 @@ namespace FoodData
             }
 
             lines = File.ReadAllLines(dir + "CONVERSION FACTOR.csv", Encoding.UTF8);
-            foreach (string line in lines)
+            for (int i = 1; i < lines.Length; i++)
             {
-                ConversionFactor cv = new ConversionFactor(line);
+                ConversionFactor cv = new ConversionFactor(lines[i]);
                 //conversionFactors.Add(cv.FoodId, cv);
                 if (!conversionFactors.ContainsKey(cv.FoodId))
                 {
@@ -42,9 +42,9 @@ namespace FoodData
             }
 
             lines = File.ReadAllLines(dir + "MEASURE NAME.csv", Encoding.UTF8);
-            foreach (string line in lines)
+            for (int i = 1; i < lines.Length; i++)
             {
-                MeasureName mn = new MeasureName(line);
+                MeasureName mn = new MeasureName(lines[i]);
                 if (!measureNames.ContainsKey(mn.MeasureId))
                 {
                     measureNames.Add(mn.MeasureId, mn);
@@ -52,9 +52,9 @@ namespace FoodData
             }
 
             lines = File.ReadAllLines(dir + "NUTRIENT AMOUNT.csv", Encoding.UTF8);
-            foreach (string line in lines)
+            for (int i = 1; i < lines.Length; i++)
             {
-                NutrientAmount na = new NutrientAmount(line);
+                NutrientAmount na = new NutrientAmount(lines[i]);
                 if (na != null && na.FoodId != 0)
                 {
                     foods[na.FoodId].nutrients.Add(na);
@@ -62,9 +62,9 @@ namespace FoodData
             }
 
             lines = File.ReadAllLines(dir + "NUTRIENT NAME.csv", Encoding.UTF8);
-            foreach (string line in lines)
+            for (int i = 1; i < lines.Length; i++)
             {
-                NutrientName nn = new NutrientName(line);
+                NutrientName nn = new NutrientName(lines[i]);
                 if (!nutrientNames.ContainsKey(nn.NutrientID))
                 {
                     nutrientNames.Add(nn.NutrientID, nn);
