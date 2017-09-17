00c75db941bf23d971177d3be21ed6903f5fc33e
Kevin Trieu <dragn194@gmail.com>
2017-09-17 00:01:25 -0400

    Add JSON.NET to FoodData

--- FoodData/Program.cs
+++ FoodData/Program.cs
@@ -15,13 +15,13 @@ namespace BroccoliScraper
         {
             FoodData data = new FoodData(@"..\..\..\cnf-fcen-csv\");
             List<DbRow> rows = new List<DbRow>();
-            foreach (var food in data.foods)
+            foreach (var food in data.foods.Values)
             {
-                if (food.Value.Description != null)
+                if (food.Description != null)
                 {
                     DbRow row = new DbRow();
-                    row.Description = food.Value.Description;
-                    Measure measure = data.getMeasure(food.Value);
+                    row.Description = food.Description;
+                    Measure measure = data.getMeasure(food);
                     if (measure == null)
                     {
                         continue;
@@ -31,8 +31,21 @@ namespace BroccoliScraper
                         continue;
                     }
                     row.MeasureType = measure.Unit;
+                    row.Nutrients = new List<Nutrient>();
+                    foreach (var nutrientAmount in food.nutrients)
+                    {
+                        var newNutrient = new Nutrient();
+                        var nutrientName = data.nutrientNames[nutrientAmount.NutrientID];
+                        newNutrient.Name = nutrientName.Name;
+                        newNutrient.Symbol = nutrientName.NutrientSymbol;
+                        newNutrient.Unit = nutrientName.NutrientUnit;
+                        newNutrient.Quantity = nutrientAmount.NutrientValue;
+                        row.Nutrients.Add(newNutrient);
+                    }
+                    rows.Add(row);
                 }
             }
+            JsonConverter
         }
     }
 }
